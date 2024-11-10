f390c8c265a2dc6164f7100e56d4049d1e39a038
devenx0812 <devansh.chachra.btech2022@sitpune.edu.in>
2024-11-11 02:34:01 +0530

    Add files via upload

---
+++ EXP9_A.cpp
@@ -0,0 +1,17 @@
+//GARV NANDWANA
+//23070123167
+//Program to illustrate pointers
+#include <iostream>
+using namespace std;
+int main (){
+    int var=20;
+    // declare pointer c=variable
+    int *ptr;
+    //note that data type of ptr and var must be same
+    ptr =&var;
+    //assign the address of variable of a variable to a pointer 
+    cout<<"Value at ptr=" << ptr <<  "\n";
+    cout<<"Value at var=" << var <<  "\n";
+    cout<<"Value at *ptr=" << *ptr <<  "\n";
+    return 0;
+}
